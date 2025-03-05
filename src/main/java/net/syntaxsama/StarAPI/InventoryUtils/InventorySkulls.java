f167134da7ebd1d452001cb4399023248841abf9
SyntaxSama <syntaxsama@gmail.com>
2025-03-04 21:51:04 -0500

    Ability to add user heads to inventories!

--- src/main/java/net/syntaxsama/StarAPI/InventoryUtils/InventorySkulls.java
+++ src/main/java/net/syntaxsama/StarAPI/InventoryUtils/InventorySkulls.java
@@ -3,6 +3,7 @@ package net.syntaxsama.StarAPI.InventoryUtils;
 import net.syntaxsama.StarAPI.MessageUtils.MessageColors;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -15,6 +16,7 @@ import java.util.Arrays;
 import java.util.UUID;
 
 public class InventorySkulls {
+
    public static void addSkullToInventory(Inventory inventory, String url, int amount, int slot, String itemName, String... lore) {
       ItemStack skullStack = new ItemStack(Material.PLAYER_HEAD, amount);
       SkullMeta skullMeta = (SkullMeta)skullStack.getItemMeta();
@@ -48,5 +50,33 @@ public class InventorySkulls {
          inventory.addItem(new ItemStack[]{skullStack});
       }
 
+   }
+
+   public static void addSkullToInventory(Inventory inventory, Player player, int amount, int slot, String itemName, String... lore) {
+      ItemStack skullStack = new ItemStack(Material.PLAYER_HEAD, amount);
+      SkullMeta skullMeta = (SkullMeta) skullStack.getItemMeta();
+
+      if (skullMeta != null) {
+         skullMeta.setOwningPlayer(player);
+
+         if (itemName != null) {
+            skullMeta.setDisplayName(itemName);
+         }
+
+         if (lore != null && lore.length > 0) {
+            for (int i = 0; i < lore.length; ++i) {
+               lore[i] = MessageColors.hex(lore[i]);
+            }
+            skullMeta.setLore(Arrays.asList(lore));
+         }
+
+         skullStack.setItemMeta(skullMeta);
+      }
+
+      if (slot >= 0 && slot < inventory.getSize()) {
+         inventory.setItem(slot, skullStack);
+      } else {
+         inventory.addItem(skullStack);
+      }
    }
 }
